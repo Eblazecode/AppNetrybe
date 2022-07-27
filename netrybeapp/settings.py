0510eca3d2fb4cd2478b948099305314f8efc209
eblazecode <dynablaze@gmail.com>
2022-07-27 12:51:14 -0400

    adjusted allowed host

--- netrybeapp/settings.py
+++ netrybeapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-ha#kt7@afoy$i5v_ha&qadc^q@$z@pvf30x)js(^!9hcafs764
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0. 0.1', 'netrybecloud.herokuapp.com']
 
 
 # Application definition
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
